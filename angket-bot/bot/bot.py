ae89be32fed24a115a6fcae0bf89041daa2600ef
Nhakach <Panhaprofit@gmail.com>
2026-08-22 12:09:05 +0700

    feat: testd

--- angket-bot/bot/bot.py
+++ angket-bot/bot/bot.py
@@ -1,9 +1,16 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.ext import (
+    Application,
+    CallbackQueryHandler,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
 
 from bot.analysis.utils import init_db
 from bot.config import TELEGRAM_BOT_TOKEN, VIRUSTOTAL_API_KEY
-from bot.handlers.file_handler import handle_file
+from bot.handlers.file_handler import handle_button_click, handle_file
 from bot.handlers.text_handler import handle_text, start
+
 
 def main():
     if not TELEGRAM_BOT_TOKEN or not VIRUSTOTAL_API_KEY:
@@ -13,12 +20,17 @@ def main():
     init_db()
     app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
+    # Handlers
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.Document.ALL, handle_file))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+    
+    # Callback query handler for interactive inline buttons
+    app.add_handler(CallbackQueryHandler(handle_button_click))
 
     print("🤖 Bot is running...")
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
